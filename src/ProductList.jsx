b5eb7b86a74f146a887b83232c815a32335c00c4
Lenora Stevens <tlbs6513@gmail.com>
2025-07-31 19:07:58 -0400

    working on product add errors

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import './ProductList.css'
+import './ProductList.css';
+import { useSelector, useDispatch } from 'react-redux';
 import CartItem from './CartItem';
 import { addItem } from './CartSlice';
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
+    const dispatch = useDispatch();
 
     const plantsArray = [
         {
@@ -255,8 +257,8 @@ function ProductList({ onHomeClick }) {
         setShowCart(false);
     };
 
-    const handleAddToCart = (e) => {
-        dispatchEvent(addItem(plant));
+    const handleAddToCart = (plant) => {
+        dispatch(addItem(plant));
         
         setAddedToCart((prevState) => ({
             ...prevState,
