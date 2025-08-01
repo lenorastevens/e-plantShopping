26f39897fb70150e2b670f024d493e3b260551d4
Lenora Stevens <tlbs6513@gmail.com>
2025-07-31 20:53:21 -0400

    updated prices and fixed typos

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -8,7 +8,7 @@ export const CartSlice = createSlice({
   reducers: {
     addItem: (state, action) => {
         const { name, image, cost } = action.payload;
-        const existingItem = state.items.find(item => item.nae === name);
+        const existingItem = state.items.find(item => item.name === name);
 
         if (existingItem) {
             existingItem.quantity++;
